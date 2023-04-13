457684d671844f2471e206e38ad821571c968ba4
MaxSvjatoha <maxsvjatoha@gmail.com>
2023-04-13 16:51:47 +0200

    minor code cleanup

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 
 import json
+import numpy as np
 
 import utils.util as utils
 
@@ -29,7 +30,7 @@ if __name__ == '__main__':
 
     # Match input file names to summary file sheet names
     matches = utils.match_lists(input_folder_names, summary_sheets, filter_doubles = True)
-    print(f"Matches: {matches}")
+    # print(f"Matches: {matches}")
 
     # Create a dictionary of input data and initialize subdicts for each input file
     input_data_dict = {}
@@ -39,7 +40,7 @@ if __name__ == '__main__':
         if input_data_key in matches.keys():
             input_data_dict[input_data_key] = {} # Initialize subdict to store sheet data
 
-    print("Processing input files: ")
+    print("Processing input files...")
     for key in input_data_dict.keys():
         wb = utils.excel_to_workbook(file)
         scope_sheets = [sheet for sheet in wb.sheetnames if 'scope' in sheet.lower()]
@@ -47,13 +48,56 @@ if __name__ == '__main__':
             input_data_dict[key][sheet] = utils._get_scope_data(wb, sheet)
         wb.close()
 
+    print("Writing data to summary file:")
     for key in input_data_dict.keys():
         # use 'matches' dict to find where to write the data
         print(f"Writing data from {key} to sheet {matches[key]['match']}")
-        # TODO: continue here
 
-    #print(f"Saving summary file {settings['Output file name']} to {settings['Output file folder path']}")
-    #summary_wb.save(os.path.join(settings['Output file folder path'], settings["Output file name"]))
+        # load sheet in summary file which matches matches[key]['match']
+        # print(f"Loading sheet {summary_wb[matches[key]['match']]} from summary file")
+        summary_sheet = summary_wb[matches[key]['match']]
+
+        match_count = 0 # Keep track of how many matches are found for a given subitem
+        match_dict = {} # Keep track of which row and column a given subitem is found in
+
+        # write data to sheet
+        for item in input_data_dict[key].keys():
+            for subitem in input_data_dict[key][item].keys():
+                # Match subitem to row and column in summary sheet
+                max_row = summary_sheet.max_row
+                max_col = summary_sheet.max_column
+                for row in range(1, max_row + 1):
+                    for col in range(1, max_col + 1):
+                        if summary_sheet.cell(row = row, column = col).value == subitem:
+                            # print(f"Found match for {subitem} at row {row} and column {col}")
+                            match_count += 1
+                            match_dict[match_count] = {"row": row, "col": col}
+                if match_count > 1:
+                    if 'Scope 1'.lower() in item.lower():
+                        write_key = min(match_dict.keys()) # use match_dict entry with the lowest key
+                    elif 'Scope 3'.lower() in item.lower():
+                        write_key = max(match_dict.keys()) # use match_dict entry with the highest key
+                    else: 
+                        pass # TODO # Handler (non-urgent)
+                elif match_count == 0:
+                    continue # go back to top of loop
+                else:
+                    write_key = list(match_dict.keys())[0] # use match_dict entry with the only key
+                
+                # Extract data from input_data_dict and write to summary sheet. If none, generate random data
+                if input_data_dict[key][item][subitem] is not None:
+                    write_data = input_data_dict[key][item][subitem] # Write data to summary sheet
+                else:
+                    write_data = np.random.randint(0, 100)
+                
+                summary_sheet.cell(row = match_dict[write_key]["row"], column = match_dict[write_key]["col"] + 1).value = write_data
+                
+                # Reset match_count and match_dict
+                match_count = 0
+                match_dict = {}
+
+    summary_wb.save(os.path.join(settings['Output file folder path'], settings["Output file name"]))
     summary_wb.close()
 
-    print("Script execution finished")
+    print("Script execution finished successfully")
+    sys.exit(0)
